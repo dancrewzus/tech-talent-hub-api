3490f7083b2f1b527d4e289c31fa469d7626e31f
Joel Abreu <joelabreurojas@gmail.com>
2024-06-01 20:34:45 -0400

    Add offer interface to return data

---
+++ src/functionalities/offer-employ/interfaces/offer-return-data.interface.ts
@@ -0,0 +1,11 @@
+export interface OfferReturnData {
+    id: any
+    title: string
+    position: string
+    yearsOfExperience: number
+    contract: string
+    keywords: string
+    salary: number
+    hiringDate: string
+    status: boolean
+}
