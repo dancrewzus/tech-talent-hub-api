c609771e4daf48fbad8d0863b7efc5e94552934c
Joel Abreu <joelabreurojas@gmail.com>
2024-06-01 20:32:09 -0400

    Add offer service with create feature

---
+++ src/functionalities/offer-employ/offer.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+import { HandleErrors } from 'src/common/utils/handleErrors.util'
+import { DayJSAdapter } from 'src/common/adapters/dayjs.adapter'
+import { ConfigService } from '@nestjs/config'
+
+import { Offer } from './entities/offer.entity'
+import { User } from '../users/entities/user.entity'
+import { Track } from '../tracks/entities/track.entity'
+import { CreateOfferDto } from './dto/create-offer.dto'
+import { OfferReturnData } from './interfaces/offer-return-data.interface'
+
+@Injectable()
+export class OfferService {
+
+    private defaultLimit: number;
+    
+    constructor(
+        @InjectModel(Offer.name, 'default') private readonly offerModel: Model<Offer>,
+        @InjectModel(User.name, 'default') private readonly userModel: PaginateModel<User>,
+        @InjectModel(Track.name, 'default') private readonly trackModel: Model<Track>,
+        private readonly configService: ConfigService,
+        private readonly dayjs: DayJSAdapter,
+        private readonly handleErrors: HandleErrors
+    ) { 
+        this.defaultLimit = this.configService.get<number>('defaultLimit')
+    }
+
+    /**
+    * Creates a new offer in the database with specified data from CreateOfferDto, along with additional
+    * audit and tracking information.
+    *
+    * @public
+    * @async
+    * @function create
+    * @param {CreateOfferDto} createOfferDto - Data transfer object containing the new offer's data.
+    * @param {User} userRequest - The user object of the requester, used to set the `createdBy` field.
+    * @param {string} clientIp - IP address from which the request originated, used for logging purposes.
+    * @returns {Promise<OfferReturnData>} A promise that resolves to the formatted offer data upon successful creation.
+    */
+    public create() = async (createOfferDto: CreateOfferDto, userRequest: User, clientIp: string): Promise<OfferReturnData> => {
+        try {
+            const offer =await this.offerModel.create({
+                createdBy: userRequest.id,
+                createdAt: this.dayjs.getCurrentDateTime(),
+                updatedAt: this.dayjs.getCurrentDateTime(),
+                ...createOfferDto
+            })
+
+            await this.trackModel.create({
+                ip: clientIp,
+                description: `Offer ${ offer._id } was created.`
+                module: 'Offers',
+                createAt: this.dayjs.getCurrentDateTime(),
+                user: userRequest.id
+            })
+            return this.formatReturnData(offer)
+        } catch (error) {
+            this.handleErrors.handleExceptions(error)
+        }
+    }
+}
